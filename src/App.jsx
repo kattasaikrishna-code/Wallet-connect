98fab729dfb3a8fa97eb3e0f1b4080d861e9fb58
Katta Saikrishna <Katta_sai@nextazy.com>
2025-11-07 12:00:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {
+  useAppKit,
+  useAppKitAccount,
+  useDisconnect,
+} from "@reown/appkit/react";
+import "./App.css";
+
+export default function App() {
+  const { open, close } = useAppKit();
+  const { address, isConnected } = useAppKitAccount();
+  console.log("user connected address", { address, isConnected });
+  const { disconnect } = useDisconnect();
+
+  return (
+    <div className="container">
+      {!isConnected ? (
+        <button className="button" onClick={() => open({ view: "Connect" })}>
+          <h1>Connect Wallet</h1>
+        </button>
+      ) : (
+        <div>
+          <h1 className="connected-text">Connected Wallet Address</h1>
+          <p className="address-text">{address}</p>
+          <button
+            className="button"
+            // onClick={() => open({ view: "Disconnect" })}
+            onClick={() => disconnect()}
+          >
+            <h1>Disconnect Wallet</h1>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/config/appProvider.jsx
@@ -0,0 +1,47 @@
+import { createAppKit } from "@reown/appkit/react";
+import { WagmiProvider } from "wagmi";
+import { arbitrum, mainnet, sepolia, bscTestnet } from "@reown/appkit/networks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId from https://dashboard.reown.com
+const projectId = "ac5c4bb54e7ac9629b0d6dbce2a116d5";
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: "AppKit",
+  description: "AppKit Example",
+  url: window.location.origin, // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+// 3. Set the networks
+const networks = [mainnet, arbitrum, sepolia, bscTestnet];
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+});
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+  },
+});
+
+export function AppKitProvider({ children }) {
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
